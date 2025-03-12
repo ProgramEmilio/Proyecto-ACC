8f824430b26215c5fdac1ccbaa5fbd92cef710ae
Belem Picos <L21170219@culiacan.tecnm.mx>
2025-03-12 16:38:55 -0700

    Diseño

---
+++ Usuarios/Usuario.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usuario</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="../CSS/menu.css" type="text/css">
+    <link rel="stylesheet" href="../CSS/cabecera.css" type="text/css">
+    <link rel="stylesheet" href="../CSS/pie_pagina.css" type="text/css">
+    <link rel="stylesheet" href="../CSS/tablas_boton.css" type="text/css">
+    <link rel="stylesheet" href="../CSS/formularios.css" type="text/css">
+    <link rel="stylesheet" href="../CSS/departamentos.css" type="text/css">
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,
+    700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+          integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+
+        <!--Iconos-->
+        <script src="https://kit.fontawesome.com/eb496ab1a0.js" crossorigin="anonymous"></script>
+</head>
+
+<header class="cabecera_p">
+    <div class="cabecera">
+        <h1 class="nom_sis">Aplica Central Creativa</h1>
+        <a href="../Menu.php"><img src="../Imagenes/acc_logo.png" class="img-logo" alt="Logo"></a>
+        <a href="#"><img src="../Imagenes/avatar.png" class="img-avatar" alt="Avatar"></a>
+    </div>
+
+    <div class="header">
+        <ul class="nav">
+                <!-- Usuarios -->
+                <li><a href="Usuario.php">Usuarios</a>
+                    <ul class="submenu">
+                        <li><a href="Registro/Registro_Usuario.php">Alta</a></li>
+                        <li><a href="Editar/Editar_Usuario.php">Modificar</a></li>
+                        <li><a href="Eliminar/Eliminar_Usuario.php">Baja</a></li>
+                    </ul>
+                </li>
+                <!-- Proveedor -->
+                <li><a href="#">Proveedor</a>
+                </li>
+
+                <!-- Ventas -->
+                <li><a href="#">Ventas</a>
+                </li>
+
+                <!-- Compras -->
+                <li><a href="#">Compras</a>
+                </li>
+
+              <!-- Inventario -->
+                <li><a href="#">Inventario</a>
+                </li>
+
+                <!-- Distribucion -->
+                <li><a href="#">Distribucion</a>
+                </li>
+
+             <!-- Produccion -->
+                <li><a href="#">Produccion</a>
+                </li>
+        </ul>
+    </div>
+</header>
+</head>
+<body>
+    
+</body>
+
+<footer class="pie-pagina">
+        <div class="grupo-2">
+            <small>&copy; 2025 <b>Aplica Central Creativa</b> - Todos los Derechos Reservados.</small>
+        </div>
+    </footer>
+</html>

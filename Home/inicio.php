a2595a5a025344ccdf276fd27ea1a79e9473998b
fersoliz27 <fersoliz0727@gmail.com>
2025-03-13 07:23:40 -0700

    Home

---
+++ Home/inicio.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ACC</title>
+
+    <!-- FUENTE GOOGLE FONTS : Poppins -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+
+
+
+    <!-- Mis Estilos -->
+    <link rel="stylesheet" href="inicio.css">
+
+</head>
+<body>
+
+    <div class="hm-wrapper">
+
+        <!-- =================================
+           HEADER MENU
+        ================================== -->
+        <div class="hm-header">
+
+            <div class="container">
+                <div class="header-menu">
+
+                    <div class="hm-logo">
+                        <a href="#">
+                            <img src="acc_logo.png" alt="">
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+        <!-- =================================
+           Productos
+        ================================== -->
+        <div class="products-container">
+            <div class="product">
+                <div class="product-img-container">
+                    <img src="playera2.png" alt="Producto 1">
+                    <a href="#" class="view-more">Ver más</a>
+                </div>
+                <p class="product-name">Playera con logo</p>
+            </div>
+            <div class="product">
+                <div class="product-img-container">
+                    <img src="termo3.png." alt="Producto 2">
+                    <a href="#" class="view-more">Ver más</a>
+                </div>
+                <p class="product-name">Termo YETI</p>
+            </div>
+            <div class="product">
+                <div class="product-img-container">
+                    <img src="agenda1.png" alt="Producto 3">
+                    <a href="#" class="view-more">Ver más</a>
+                </div>
+                <p class="product-name">Agenda 2025</p>
+            </div>
+        </div>
+
+         <!-- =================================
+           FOOTER
+        ================================== -->
+        <footer>
+
+        <div class="foo-copy">
+            <div class="container">
+                <p>ACC 2025 © Todos los derechos reservados</p>
+            </div>
+        </div>
+
+        </footer>
+
+        
+
+</body>
+</html>
